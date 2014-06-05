f0aec4338835119b5646eea18e0896d24ac1553d
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-06-05 16:00:12 +0200

    updated the image path and added a sample picture for amsterdam 😎

--- sql/test_data/data_community_leaders_amsterdam.sql
+++ sql/test_data/data_community_leaders_amsterdam.sql
@@ -2,4 +2,4 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 TRUNCATE TABLE `community_leader`;
 
-INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `order`) VALUES(1, 'Giovanni Maggini', 'CfE Fellow', 'images/leaders/philly/maggini.jpg', 1);
+INSERT INTO `community_leader` (`id`, `display_name`, `title`, `image_path`, `order`) VALUES(1, 'Giovanni Maggini', 'CfE Fellow', 'images/leaders/amsterdam/maggini.jpg', 1);
