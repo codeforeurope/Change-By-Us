57ae0d12a9964c2ca7c39130b1f59e5dc52dad36
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-07-28 17:33:22 +0200

    reformatted file. Added html version of email messages

--- giveaminute/messaging.py
+++ giveaminute/messaging.py
@@ -13,10 +13,10 @@ from framework.emailer import Emailer
 from framework.log import log
 from framework.config import Config
 
-## EMAIL FUNCTIONS
+# # EMAIL FUNCTIONS
 
 # send email to invited users
-def emailInvite(email, inviterName, projectId, title, description, message = None):
+def emailInvite(email, inviterName, projectId, title, description, message=None):
     """
     Send invitation email.  Using template: project_invite
         
@@ -28,32 +28,33 @@ def emailInvite(email, inviterName, projectId, title, description, message = Non
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     subject = "You've been invited by %s to join a project" % inviterName
     link = "%sproject/%s" % (Config.get('default_host'), str(projectId))
     template_values = {
         'inviter': inviterName,
-        'title':title,
-        'description':description,
+        'title': title,
+        'description': description,
         'link': link,
         'message': message,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/project_invite', template_values, suffix = 'txt')     
-    
+    body = Emailer.render('email/project_invite', template_values, suffix='txt')
+    html = Emailer.render('email/project_invite', template_values, suffix='html')
+
     # Send email.
     try:
-        return Emailer.send(email, subject,  body, from_name = emailAccount['from_name'], 
-            from_address = emailAccount['from_email'])  
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send invite email")
         log.error(e)
         return False
-        
+
 
 def emailProjectJoin(email, projectId, title, userId, userName):
     """
@@ -67,7 +68,7 @@ def emailProjectJoin(email, projectId, title, userId, userName):
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     defaultUrl = Config.get('default_host')
@@ -81,20 +82,21 @@ def emailProjectJoin(email, projectId, title, userId, userName):
         'member_link': memberLink,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/project_join', template_values, suffix = 'txt')
-         
+    body = Emailer.render('email/project_join', template_values, suffix='txt')
+    html = Emailer.render('email/project_join', template_values, suffix='html')
+
     # Send email.
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send join email")
         log.error(e)
         return False
 
-      
+
 def emailProjectEndorsement(email, title, leaderName):
     """
     Email project admins about endorsements.  Using template: project_endorsement
@@ -107,7 +109,7 @@ def emailProjectEndorsement(email, title, leaderName):
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     subject = "%s liked your project!" % leaderName
@@ -116,20 +118,21 @@ def emailProjectEndorsement(email, title, leaderName):
         'leader_name': leaderName,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/project_endorsement', template_values, suffix = 'txt')
-         
+    body = Emailer.render('email/project_endorsement', template_values, suffix='txt')
+    html = Emailer.render('email/project_endorsement', template_values, suffix='html')
+
     # Send email.
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send endorsement email")
         log.error(e)
         return False
 
-        
+
 def emailResourceNotification(email, projectId, title, description, resourceName):
     """
     Email resource contacts on resource add.  Using template: resource_notification
@@ -142,7 +145,7 @@ def emailResourceNotification(email, projectId, title, description, resourceName
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     subject = "A project on Changeby.us has added %s as a resource" % resourceName
@@ -154,10 +157,11 @@ def emailResourceNotification(email, projectId, title, description, resourceName
         'link': link,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/resource_notification', template_values, suffix = 'txt')
-    
+    body = Emailer.render('email/resource_notification', template_values, suffix='txt')
+    html = Emailer.render('email/resource_notification', template_values, suffix='html')
+
     # If dev, don't email resources
     if (Config.get('dev')):
         log.info("*** body = %s" % body)
@@ -165,13 +169,13 @@ def emailResourceNotification(email, projectId, title, description, resourceName
 
     # Send email.
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send resource notification email")
         log.error(e)
         return False
-      
+
 
 def emailResourceApproval(email, title):
     """
@@ -185,7 +189,7 @@ def emailResourceApproval(email, title):
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     subject = "Your resource has been approved"
@@ -194,19 +198,20 @@ def emailResourceApproval(email, title):
         'title': title,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/resource_approval', template_values, suffix = 'txt')
+    body = Emailer.render('email/resource_approval', template_values, suffix='txt')
+    html = Emailer.render('email/resource_approval', template_values, suffix='html')
 
     # Send email.
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])  
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send resource approval email")
         log.error(e)
         return False
-        
+
 
 def emailAccountDeactivation(email):
     """
@@ -220,7 +225,7 @@ def emailAccountDeactivation(email):
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     subject = "Your account has been deactivated"
@@ -229,14 +234,15 @@ def emailAccountDeactivation(email):
         'link': link,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/account_deactivation', template_values, suffix = 'txt')
+    body = Emailer.render('email/account_deactivation', template_values, suffix='txt')
+    html = Emailer.render('email/account_deactivation', template_values, suffix='html')
 
     # Send email.
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send account deactivation email")
         log.error(e)
@@ -255,7 +261,7 @@ def emailTempPassword(email, password):
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     subject = "Your password has been reset"
@@ -266,14 +272,15 @@ def emailTempPassword(email, password):
         'link': link,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/forgot_password', template_values, suffix = 'txt')
+    body = Emailer.render('email/forgot_password', template_values, suffix='txt')
+    html = Emailer.render('email/forgot_password', template_values, suffix='html')
 
     # Send email.
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send forgot password email")
         log.error(e)
@@ -292,7 +299,7 @@ def directMessageUser(db, toUserId, toName, toEmail, fromUserId, fromName, messa
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     #email = "%s <%s>" % (toName, toEmail)
@@ -305,17 +312,18 @@ def directMessageUser(db, toUserId, toName, toEmail, fromUserId, fromName, messa
         'link': link,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/direct_message', template_values, suffix = 'txt')
+    body = Emailer.render('email/direct_message', template_values, suffix='txt')
+    html = Emailer.render('email/direct_message', template_values, suffix='html')
 
     # Send email.
     try:
-        isSent = Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])
-                                                       
+        isSent = Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                              from_address=emailAccount['from_email'])
+
         if (isSent):
-            db.insert('direct_message', message = message, to_user_id = toUserId, from_user_id = fromUserId)
+            db.insert('direct_message', message=message, to_user_id=toUserId, from_user_id=fromUserId)
             return True
         else:
             log.info("*** couldn't log direct message")
@@ -326,7 +334,7 @@ def directMessageUser(db, toUserId, toName, toEmail, fromUserId, fromName, messa
         log.info("*** couldn't send direct message email")
         log.error(e)
         return False
-        
+
 
 def emailUnauthenticatedUser(email, authGuid):
     """
@@ -340,7 +348,7 @@ def emailUnauthenticatedUser(email, authGuid):
     @returns: Emailer send response.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     subject = "Please authenticate your account"
@@ -349,20 +357,21 @@ def emailUnauthenticatedUser(email, authGuid):
         'link': link,
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/auth_user', template_values, suffix = 'txt')
-            
+    body = Emailer.render('email/auth_user', template_values, suffix='txt')
+    html = Emailer.render('email/auth_user', template_values, suffix='html')
+
     # Send email.            
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])  
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send authenticate user email")
         log.error(e)
         return False
 
-        
+
 def emailIdeaConfirmation(email, responseEmail, locationId):
     """
     Email upon idea submission.  Using template: idea_confirmation
@@ -375,7 +384,7 @@ def emailIdeaConfirmation(email, responseEmail, locationId):
     @returns: Whether emailer was successful or not.
     
     """
-    
+
     # Create values for template.
     emailAccount = Config.get('email')
     host = Config.get('default_host')
@@ -388,14 +397,15 @@ def emailIdeaConfirmation(email, responseEmail, locationId):
         'response_email': emailAccount['from_email'],
         'config': Config.get_all()
     }
-    
+
     # Render email body.
-    body = Emailer.render('email/idea_confirmation', template_values, suffix = 'txt')
+    body = Emailer.render('email/idea_confirmation', template_values, suffix='txt')
+    html = Emailer.render('email/idea_confirmation', template_values, suffix='html')
 
     # Send email.
     try:
-        return Emailer.send(email, subject, body, from_name = emailAccount['from_name'],
-            from_address = emailAccount['from_email'])
+        return Emailer.send(email, subject, body, html, from_name=emailAccount['from_name'],
+                            from_address=emailAccount['from_email'])
     except Exception, e:
         log.info("*** couldn't send authenticate user email")
         log.error(e)
@@ -403,52 +413,55 @@ def emailIdeaConfirmation(email, responseEmail, locationId):
 
 
 ### SMS FUNCTIONS
-        
+
 # add phone number to table of stopped numbers
 def stopSMS(db, phone):
     try:
-        db.insert('sms_stopped_phone', phone = phone)
+        db.insert('sms_stopped_phone', phone=phone)
         return True
     except Exception, e:
         log.info("*** couldn't stop messages to phone number %s.  Number may already be in database." % phone)
         log.error(e)
         return False
-        
+
+
 def isPhoneStopped(db, phone):
     try:
         sql = "select phone from sms_stopped_phone where phone = $phone limit 1";
-        data = list(db.query(sql, {'phone':phone}))
-        
+        data = list(db.query(sql, {'phone': phone}))
+
         return len(data) > 0
     except Exception, e:
         log.info("*** couldn't get sms stopped value for %s" % phone)
         log.error(e)
-        
+
         # in this case, we err on NOT sending messages and thus return True
         return True
 
+
 def sendSMSConfirmation(db, phone):
     log.info("*** sending confirmation to %s" % phone)
-    
+
     if (not isPhoneStopped(db, phone)):
-        message = "Thanks for adding your idea to changeby.us Visit %smobile to browse and join projects related to your idea." % Config.get('default_host')
-        
+        message = "Thanks for adding your idea to changeby.us Visit %smobile to browse and join projects related to your idea." % Config.get(
+            'default_host')
+
         return helpers.sms.send(phone, message)
     else:
         return False
-    
+
+
 def sendSMSInvite(db, phone, projectId):
-    log.info("*** sending invite to %s" % phone)  
-    
+    log.info("*** sending invite to %s" % phone)
+
     try:
         if (not isPhoneStopped(db, phone)):
             link = "%sproject/%s" % (Config.get('default_host'), str(projectId))
-            message = "You've been invited to a project on changeby.us. Visit %s to see the project. Reply 'STOP' to stop changeby.us messages." % link        
+            message = "You've been invited to a project on changeby.us. Visit %s to see the project. Reply 'STOP' to stop changeby.us messages." % link
             return helpers.sms.send(phone, message)
         else:
-            return False    
+            return False
     except Exception, e:
         log.info("*** something failed in sending sms invite")
         log.error(e)
         return False    
-    
