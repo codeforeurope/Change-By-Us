d1bbf2d0171fe32ebea488c434e34079978b06c4
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-11-21 16:58:07 +0100

    formatted file and using LOCALE to format date

--- framework/util.py
+++ framework/util.py
@@ -6,8 +6,12 @@
 # generally try not to import things up here
 import re, base64, string, urlparse, datetime, json, jinja2
 from framework.log import log
+from framework.config import Config
+import locale
 
 _TO_UNICODE_TYPES = (unicode, type(None))
+
+
 def to_unicode(value):
     """Converts a string argument to a unicode string.
 
@@ -19,13 +23,14 @@ def to_unicode(value):
     assert isinstance(value, bytes)
     return value.decode("utf-8")
 
+
 def recursive_unicode(obj):
     """Walks a simple data structure, converting byte strings to unicode.
 
     Supports lists, tuples, and dictionaries.
     """
     if isinstance(obj, dict):
-        return dict((recursive_unicode(k), recursive_unicode(v)) for (k,v) in obj.iteritems())
+        return dict((recursive_unicode(k), recursive_unicode(v)) for (k, v) in obj.iteritems())
     elif isinstance(obj, list):
         return list(recursive_unicode(i) for i in obj)
     elif isinstance(obj, tuple):
@@ -36,6 +41,7 @@ def recursive_unicode(obj):
         if isinstance(obj, unicode) or isinstance(obj, basestring):
             obj = jinja2.Markup(obj).unescape()
         return obj
+
 
 class EscapingJSONEncoder(json.JSONEncoder):
     def encode(self, obj):
@@ -52,20 +58,22 @@ def try_f(f, data, default=None):
     except Exception, e:
         return default
 
+
 def dictsort(dict_list, sort_by_me):
     """
     Takes a list of dicts and returns that list sorted by the property given in
     the sort_by_me argument.
     """
-    decorated = [(resolve_variable('var.' + sort_by_me, {'var' : item}), item) for item in dict_list]
+    decorated = [(resolve_variable('var.' + sort_by_me, {'var': item}), item) for item in dict_list]
     decorated.sort()
     return [item[1] for item in decorated]
+
 
 def safeuni(s):
     """
     Tries to convert an object to a unicode string, handling common edge cases.
     """
-    #if unicode
+    # if unicode
     if isinstance(s, unicode):
         return s
 
@@ -86,13 +94,14 @@ def safeuni(s):
         s = unicode(s, errors='ignore', encoding='utf-8')
     return s
 
+
 def safestr(s):
     """
     Tries to convert an object to a string, handling common edge cases. Also
     supports iterators by converting all items to strings.
     """
 
-    #return if this is a string already
+    # return if this is a string already
     if isinstance(s, str):
         return s
 
@@ -106,11 +115,13 @@ def safestr(s):
         return s
 
     #if an iterater, then try to convert everything to a string
-    if hasattr(s, 'next') and hasattr(s, '__iter__'): # iterator
+    if hasattr(s, 'next') and hasattr(s, '__iter__'):  # iterator
         import itertools
+
         return itertools.imap(safestr, s)
     else:
         return str(s)
+
 
 def validate_email(emailaddress):
     """
@@ -118,25 +129,25 @@ def validate_email(emailaddress):
     valid, False when invalid.
     """
     domains = ["aero", "asia", "biz", "cat", "com", "coop", \
-        "edu", "gov", "info", "int", "jobs", "mil", "mobi", "museum", \
-        "name", "net", "org", "pro", "tel", "travel", "fm", "ly", "uk", \
-        "in", "us", "il", "de", "it", "fr"
-        ]
+               "edu", "gov", "info", "int", "jobs", "mil", "mobi", "museum", \
+               "name", "net", "org", "pro", "tel", "travel", "fm", "ly", "uk", \
+               "in", "us", "il", "de", "it", "fr"
+    ]
 
-    #validate length
+    # validate length
     if len(emailaddress) < 6:
         # TODO: SR: Why? i@u.nu is valid!
-        return False # Address too short.
+        return False  # Address too short.
 
     try:
         localpart, domainname = emailaddress.rsplit('@', 1)
         host, toplevel = domainname.rsplit('.', 1)
     except ValueError:
-        return False # Address does not have enough parts.
+        return False  # Address does not have enough parts.
 
     #if not a country code and not in the domain list
     if len(toplevel) != 2 and toplevel not in domains:
-        return False # Not a domain name.
+        return False  # Not a domain name.
 
     #is there a username?
     if len(localpart) == 0:
@@ -159,6 +170,7 @@ def validate_email(emailaddress):
 
     return True
 
+
 def validateUSPhone(phone):
     """
     Validates that the given string is a valid US phone number, a number
@@ -166,6 +178,7 @@ def validateUSPhone(phone):
     http://en.wikipedia.org/wiki/North_American_Numbering_Plan
     """
     return not (re.match("^[2-9]\d{9}$", phone) == None)
+
 
 def cleanUSPhone(phone):
     """
@@ -183,18 +196,19 @@ def cleanUSPhone(phone):
     else:
         return None
 
+
 def parse_tags(tagstring):
     """
     Parses out a list of tags from the given delimited tag string.
     """
-    #extract everything in quotes
+    # extract everything in quotes
     quotes = re.findall(r'".*?"', tagstring)
     for q in quotes:
-        repaired = q.replace(',', '@@') # protect commas
-        repaired = repaired.replace(' ', '$$') # protect spaces
+        repaired = q.replace(',', '@@')  # protect commas
+        repaired = repaired.replace(' ', '$$')  # protect spaces
         tagstring = tagstring.replace(q, repaired)
 
-    tagstring = tagstring.replace(',',' ')
+    tagstring = tagstring.replace(',', ' ')
     tags = tagstring.split(' ')
 
     for tag in tags[:]:
@@ -205,17 +219,20 @@ def parse_tags(tagstring):
         tags[tags.index(tag)] = t.replace('$$', ' ')
     return tags
 
+
 def list_to_str(tags):
     """
     Convert a list of tags into a delimited string.
     """
     return " ".join(safestr(iter(tags)))
 
+
 def wordcount(s):
     """
     Counts all of the words in a string.
     """
     return len(s.split())
+
 
 def filesizeformat(bytes):
     """
@@ -234,6 +251,7 @@ def filesizeformat(bytes):
         return "%.1f MB" % (bytes / (1024 * 1024))
     return "%.1f GB" % (bytes / (1024 * 1024 * 1024))
 
+
 def strip_html(s):
     """
     Remove anything between <angle brackets>.
@@ -241,12 +259,14 @@ def strip_html(s):
     p = re.compile(r'<.*?>')
     return p.sub('', s)
 
+
 def singlespace(s):
     """
     Replace all groups of white space with a single space.
     """
     p = re.compile(r'\s+')
     return p.sub(' ', s)
+
 
 def remove_linebreaks(s):
     """
@@ -257,6 +277,7 @@ def remove_linebreaks(s):
     s = ' '.join(s)
     return singlespace(s).strip()
 
+
 def depunctuate(s, exclude=None, replacement=''):
     """
     Strips out all of the punctuation of the given string, excluding any punctuation
@@ -264,6 +285,7 @@ def depunctuate(s, exclude=None, replacement=''):
     or the given replacement argument.
     """
     import string
+
     p = string.punctuation
     if exclude:
         for c in exclude:
@@ -271,11 +293,13 @@ def depunctuate(s, exclude=None, replacement=''):
     regex = re.compile('[%s]' % re.escape(p))
     return regex.sub(replacement, s)
 
+
 def nl2br(s):
     """
     Convert all newlines (\n) to <br /> tags.
     """
     return '<br />\n'.join(s.split('\n'))
+
 
 def br2nl(s):
     """
@@ -283,17 +307,20 @@ def br2nl(s):
     """
     return '\n'.join(re.split('<br ?\/?>', s))
 
+
 def prefix(delim, s):
     """
     Get the prefix of the given string, split on the given delimiter.
     """
     return s.split(delim)[0]
 
+
 def suffix(delim, s):
     """
     Get the suffix of the given string, split on the given delimiter.
     """
     return s.split(delim)[-1]
+
 
 def urlencode(s):
     """
@@ -303,7 +330,9 @@ def urlencode(s):
     if isinstance(s, unicode):
         s = s.encode('utf-8')
     import urllib
+
     return urllib.quote(s)
+
 
 def add_leading_slash(s):
     """
@@ -315,6 +344,7 @@ def add_leading_slash(s):
     if s[0] is not '/':
         s = '/' + s
     return s
+
 
 def titlecase(value):
     """
@@ -331,16 +361,17 @@ def location_cap(location):
     if not location:
         return None
 
-    #Split on comma - Fort Collins, CO, USA
+    # Split on comma - Fort Collins, CO, USA
     tokens = location.split(',')
     for token in tokens:
         #Split on spaces so we can case every word
         #If the word len > 2 (ie, not a state prefix) and not USA
-            #Titlecase the string
+        #Titlecase the string
         #Else uppercase the string
         t = [i.title() if len(i) > 2 and i.upper() != "USA" else i.upper() for i in token.split(' ')]
         tokens[tokens.index(token)] = ' '.join(t)
     return ','.join(tokens)
+
 
 def pluralize(value, arg='s'):
     """
@@ -358,15 +389,16 @@ def pluralize(value, arg='s'):
     try:
         if int(value) != 1:
             return plural_suffix
-    except ValueError: # invalid string that's not a number
+    except ValueError:  # invalid string that's not a number
         pass
-    except TypeError: # value isn't a string or a number; maybe it's a list?
+    except TypeError:  # value isn't a string or a number; maybe it's a list?
         try:
             if len(value) != 1:
                 return plural_suffix
-        except TypeError: # len() of unsized object
+        except TypeError:  # len() of unsized object
             pass
     return singular_suffix
+
 
 def slugify(value):
     """
@@ -374,6 +406,7 @@ def slugify(value):
     """
     value = re.sub('[^\w\s-]', '', value).strip().lower()
     return re.sub('[-\s]+', '-', value)
+
 
 def short_decimal(value):
     """
@@ -383,6 +416,7 @@ def short_decimal(value):
     value = int(value)
     value = float(value) / 100.0
     return value
+
 
 def zeropad(value):
     """
@@ -394,13 +428,16 @@ def zeropad(value):
     else:
         return str(value)
 
+
 def random_string(length):
     """
     Returns a random string, including upper and lower case characters and digits,
     of the specified length.
     """
     import random
+
     return ''.join(random.sample("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", length))
+
 
 def obfuscate(id, length=12):
     """
@@ -408,8 +445,10 @@ def obfuscate(id, length=12):
     character length (defaults to 12), then base 64 encodes the string and returns it.
     """
     id = str(id)
-    padding = "".join(["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"[(i + int(id)) % 52] for i in range(length - len(id))])
+    padding = "".join(
+        ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"[(i + int(id)) % 52] for i in range(length - len(id))])
     return base64.b64encode(id + padding)
+
 
 def deobfuscate(token):
     """
@@ -426,6 +465,7 @@ def deobfuscate(token):
             numbers.append(token[i])
     return "".join(numbers)
 
+
 def total_seconds(timedelta):
     """
     Return the total number of seconds in a datetime.timedelta object. This is a
@@ -435,6 +475,7 @@ def total_seconds(timedelta):
     """
     seconds = timedelta.seconds + (timedelta.days * 24 * 60 * 60)
     return seconds
+
 
 def local_utcoffset(localtime=datetime.datetime.now(),
                     utctime=datetime.datetime.utcnow()):
@@ -446,6 +487,7 @@ def local_utcoffset(localtime=datetime.datetime.now(),
     seconds = total_seconds(localtime - utctime)
     hours = int(round(float(seconds) / 60 / 60))
     return hours
+
 
 def format_time(seconds):
     """
@@ -470,12 +512,15 @@ def format_time(seconds):
 
     return time
 
+
 def good_decimal(num):
     """
     Rounds a number to the nearest two decimal places.
     """
     from decimal import Decimal
+
     return str(Decimal(str(num)).quantize(Decimal('.01')))
+
 
 def normalize(num, min, max):
     """
@@ -483,11 +528,13 @@ def normalize(num, min, max):
     """
     return (float(num) - float(min)) / (float(max) - float(min))
 
+
 def confirm_pid(run_folder):
     """
     TBD
     """
     import sys, os, signal, __main__
+
     name = prefix('.', os.path.basename(__main__.__file__))
     log.info("Attempting to launch daemon %s..." % name)
     pid = str(os.getpid())
@@ -514,7 +561,10 @@ def confirm_pid(run_folder):
     open(pidfile, 'w').write(pid)
     log.info("--> launched with pid %s" % pid)
 
+
 """ web.py specific """
+
+
 def get_flash_upload(web):
     """
     Reformat data coming in from Flash FileReference
@@ -523,6 +573,7 @@ def get_flash_upload(web):
     http://www.mail-archive.com/webpy@googlegroups.com/msg04505.html
     """
     import os
+
     tmpfile = os.tmpfile()
     contentLength = int(web.ctx.env['CONTENT_LENGTH'])
     if contentLength <= 0:
@@ -540,6 +591,7 @@ def get_flash_upload(web):
     tmpfile.close()
     return input
 
+
 def get_post_data(web):
     """
     Get either web.input['file'] (HTML multipart form) or web.data() (octet upload)
@@ -551,8 +603,9 @@ def get_post_data(web):
         try:
             data = web.data()
         except Error, e2:
-            log.error("--> bad post data web.input[%s] web.data[%s]" %s (e1, e2))
+            log.error("--> bad post data web.input[%s] web.data[%s]" % s(e1, e2))
     return data
+
 
 def get_fake_session(controller):
     """
@@ -560,6 +613,7 @@ def get_fake_session(controller):
     Flash cant consistently get cookie data.
     """
     import os, base64, pickle
+
     session_id = controller.request('session_id')
     path = "sessions/%s" % session_id
     if not os.path.exists(path):
@@ -576,11 +630,13 @@ def get_fake_session(controller):
     log.info("FAKE SESSION: %s" % fake_session)
     return fake_session
 
+
 def save_fake_session(data):
     """
         Save the session manually from a dict
     """
     import os, base64, pickle
+
     try:
         path = "sessions/%s" % data['session_id']
         pickled = pickle.dumps(data)
@@ -598,6 +654,7 @@ def save_fake_session(data):
     except IOError:
         pass
 
+
 def check_bad_words(data):
     """
         Check input against a bad words list
@@ -611,19 +668,21 @@ def check_bad_words(data):
             w = w + mod
             if data == w:
                 return True
-            if data.startswith(w+" "):
+            if data.startswith(w + " "):
                 return True
-            if data.endswith(" "+w):
+            if data.endswith(" " + w):
                 return True
-            if " "+w+" " in data:
+            if " " + w + " " in data:
                 return True
     return False
+
 
 def strNullOrEmpty(s):
     """
     Is the None, empty, or only stripable white space?
     """
     return not s or len(s.strip()) == 0
+
 
 def makeUrlAbsolute(url):
     """
@@ -642,6 +701,7 @@ def makeUrlAbsolute(url):
     else:
         return url
 
+
 def uniqify(seq):
     """
     Make a list unique.
@@ -650,6 +710,7 @@ def uniqify(seq):
     for e in seq:
         keys[e] = 1
     return keys.keys()
+
 
 def flatten(l, ltypes=(list, tuple)):
     '''
@@ -671,31 +732,20 @@ def flatten(l, ltypes=(list, tuple)):
         i += 1
     return ltype(l)
 
+
 def getBit(i, index):
     return ((i & (1 << index)) != 0)
 
+
 def setBit(i, index):
     mask = 1 << index
-    return(i | mask)
+    return (i | mask)
+
 
 def make_pretty_date(raw_date):
-    """Returns dates that end in '1st' or '22nd' and the like."""
+    """Returns dates that end in '1st' or '22nd' and the like using Python's functions."""
+
+    lang = Config.get('default_lang')
+    locale.setlocale(locale.LC_TIME, lang)
     display_date = raw_date.strftime('%B %d')
-
-    # Get rid of the 0 in single-digit days (e.g. "May 02" -> "May 2")
-    if display_date[-2] == '0':
-        display_date = display_date[:-2] + display_date[-1]
-
-    # Add on the suffix
-    if display_date[-2] == '1':
-        display_date += 'th'
-    elif display_date[-1] == '1':
-        display_date += 'st'
-    elif display_date[-1] == '2':
-        display_date += 'nd'
-    elif display_date[-1] == '3':
-        display_date += 'rd'
-    else:
-        display_date += 'th'
-
     return display_date
