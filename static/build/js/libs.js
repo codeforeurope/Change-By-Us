c8da3be3c2f190923a18383f2aff5cdadeaad97c
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-12-03 18:13:20 +0100

    admins can now make resources official/unoffical at a later time, or delete them [Fixes #83708182]

--- static/build/js/libs.js
+++ static/build/js/libs.js
@@ -3912,14 +3912,14 @@ function prettyDate(time){
 		return;
 			
 	return day_diff == 0 && (
-			diff < 60 && "just now" ||
-			diff < 120 && "1 minute ago" ||
-			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-			diff < 7200 && "1 hour ago" ||
-			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-		day_diff == 1 && "Yesterday" ||
-		day_diff < 7 && day_diff + " days ago" ||
-		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+			diff < 60 && app_page.general_messages['just-now'] ||
+			diff < 120 && app_page.general_messages['minute-ago'] ||
+			diff < 3600 && Math.floor( diff / 60 ) + " " + app_page.general_messages['minutes-ago'] ||
+			diff < 7200 && app_page.general_messages["hour-ago"] ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " " + app_page.general_messages['hours-ago']) ||
+		day_diff == 1 && app_page.general_messages['yesterday'] ||
+		day_diff < 7 && day_diff + " " + app_page.general_messages['days-ago'] ||
+		day_diff < 31 && Math.ceil( day_diff / 7 ) + " " + app_page.general_messages["weeks-ago"];
 }
 
 // If jQuery is included in the page, adds a jQuery plugin to handle it as well
