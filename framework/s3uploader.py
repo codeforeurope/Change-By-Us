b2695b8c104873a9b18ac3bd3642cb3314dcd375
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-12-11 18:14:08 +0100

    switched to both for S3 upload. Removed old library

--- framework/s3uploader.py
+++ framework/s3uploader.py
@@ -3,37 +3,40 @@
     :license: Affero GNU GPL v3, see LICENSE for more details.
 """
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import mimetypes
 import os.path
 import sys
+
 sys.path.append(os.path.dirname(__file__) + "/../")
 from framework.config import *
 from framework.log import log
-import lib.S3 as S3
+from boto.s3.connection import S3Connection
 
 class S3Uploader():
-
     @classmethod
     def upload(cls, source, destination):
         aws_config = Config.get('aws')
-        conn = S3.AWSAuthConnection(aws_config['access_key_id'], aws_config['secret_access_key'], True, aws_config['server'])
+        conn = S3Connection(aws_config['access_key_id'], aws_config['secret_access_key'])
         print source
         if source == '.' or not os.path.isfile(source):
             log.error("file not found (%s)" % source)
             return False
-        filedata = open(source, 'rb').read()
+        filehandle = open(source, 'rb')
         content_type = mimetypes.guess_type(source)[0]
         if not content_type:
             content_type = 'text/plain'
         log.info("Uploading %s to %s/%s" % (source, aws_config['bucket'], destination))
-        response = conn.put(aws_config['bucket'], destination, S3.S3Object(filedata), {'x-amz-acl': 'public-read', 'Content-Type': content_type})
-        if response.http_response.status > 299:
-            log.error("--> %s" % response.message)
-        else:
-            log.info("--> %s" % response.message)
-        return response.message
+        bucket = conn.get_bucket(aws_config['bucket'])
+        k = bucket.new_key(destination)
+        k.set_metadata('Content-Type', content_type)
+        number_of_bytes_written = k.set_contents_from_file(filehandle, policy='public-read')
+        k.set_acl('public-read')
+        if number_of_bytes_written == 0:
+            log.error("--> Could not upload to S3!")
+        return number_of_bytes_written
+
 
 if __name__ == "__main__":
     try:
